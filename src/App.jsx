4c6777b7ab03cdcc4b714090f3d8e05dfc385de4
Andres Melara <andres.melara.t@gmail.com>
2023-02-06 23:20:22 -0500

    Switch div tags to react fragments

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import Main from "./components/Main";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <h1 className="my-4 text-center text-3xl font-extrabold">
         Scoreboard App
       </h1>
       <Main />
-    </div>
+    </>
   );
 }
 
