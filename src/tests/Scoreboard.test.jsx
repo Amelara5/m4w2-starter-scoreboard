e4b6f0df7c2e49ae6bf38a81ba057d081129b8f2
Andres Melara <andres.melara.t@gmail.com>
2023-03-08 10:00:24 -0500

    Add test for checking minutes and seconds format

--- src/tests/Scoreboard.test.jsx
+++ src/tests/Scoreboard.test.jsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { expect, test } from "vitest";
 // import App from "../App";
 import Scoreboard from "../components/Scoreboard";
@@ -11,4 +11,12 @@ import Scoreboard from "../components/Scoreboard";
 test("Create a snapshot test for Scoreboard", () => {
   const scoreBoardRender = render(<Scoreboard />);
   expect(scoreBoardRender).toMatchSnapshot();
+});
+
+it("shows properly formatted minutes and seconds", () => {
+  render(<Scoreboard timeRemaining={899} />);
+
+  const timeP = screen.getByTestId("time");
+
+  expect(timeP).toHaveTextContent("14:59");
 });
