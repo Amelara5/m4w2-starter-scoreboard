45b770ffba4dc7600e6a72118bb304446dd56cc9
Andres Melara <andres.melara.t@gmail.com>
2023-03-29 19:57:24 -0400

    Add test to check if both scores updates

--- src/tests/Display.test.jsx
+++ src/tests/Display.test.jsx
@@ -21,3 +21,30 @@ it("updates the Away score", async () => {
 
   expect(awayScore).toHaveTextContent(1);
 });
+
+it("updates the Home and Away score based on the toggles", async () => {
+  const user = userEvent.setup();
+  render(<Display buttons={[0]} periods={2} timePerPeriod={3} />);
+
+  const toggleButton = screen.getByRole("checkbox");
+  const homeScore = screen.getByTestId("home-score");
+  const awayScore = screen.getByTestId("away-score");
+  const addPoint = screen.getByRole("button", { name: "1" });
+
+  //  First add a point to Away
+  await user.click(addPoint);
+  //  Second switch the toggle to add a point to Home
+  await user.click(toggleButton);
+  await user.click(addPoint);
+  //  Finally switch back to Away and a point
+  await user.click(toggleButton);
+  await user.click(addPoint);
+
+  expect(awayScore).toHaveTextContent(2);
+  expect(homeScore).toHaveTextContent(1);
+  // console.log(toggleButton);
+  // console.log(homeScore);
+  // console.log(awayScore);
+});
+
+it("advances the period only up to the max periods", async () => {});
